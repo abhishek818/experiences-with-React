6d0b2a97d490a520a8366e33029f9ad4ae789214
unknown <abhishekguptaatweb17@gmail.com>
2018-12-07 01:17:00 +0530

    add all files

---
+++ app2_Books/ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,7 @@
+export default function() {
+    return [
+        { title : 'javascript: the good parts', pages: 101},
+        { title : 'the dark knight', pages:343},
+        { title : 'wings of fire', pages:99}
+    ]
+};
